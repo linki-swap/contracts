f82ef3c6253e026bd4e713d440e8f12915ed2674
Gino <ginowine1@gmail.com>
2023-11-10 16:55:06 +0100

    create variables and event

---
+++ contracts/ccip_token_transfer/LinkiSwapTokenSender.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {OwnerIsCreator} from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/token/ERC20/IERC20.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+
+
+contract LinkiSwapTokenSender is OwnerIsCreator {
+    IRouterClient router;
+    LinkTokenInterface linkToken;
+    
+    mapping(uint64 => bool) public whitelistedChains;
+    
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
+    error DestinationChainNotWhitelisted(uint64 destinationChainSelector);
+    error NothingToWithdraw();
+    
+    event TokensTransferred(
+        bytes32 indexed messageId,
+        uint64 indexed destinationChainSelector,
+        address receiver,
+        address token,
+        uint256 tokenAmount,
+        address feeToken,
+        uint256 fees
+    );
+    
+    modifier onlyWhitelistedChain(uint64 _destinationChainSelector) {
+        if (!whitelistedChains[_destinationChainSelector])
+            revert DestinationChainNotWhitelisted(_destinationChainSelector);
+        _;
+    }
+}
