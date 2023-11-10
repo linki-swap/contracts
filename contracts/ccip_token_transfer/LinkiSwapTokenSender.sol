32bd5e39eebaf571edefb7e63d916ece829af3b6
Gino <ginowine1@gmail.com>
2023-11-10 16:57:26 +0100

    implement functions

--- contracts/ccip_token_transfer/LinkiSwapTokenSender.sol
+++ contracts/ccip_token_transfer/LinkiSwapTokenSender.sol
@@ -33,4 +33,71 @@ contract LinkiSwapTokenSender is OwnerIsCreator {
             revert DestinationChainNotWhitelisted(_destinationChainSelector);
         _;
     }
+
+    constructor(address _router, address _link) {
+        router = IRouterClient(_router);
+        linkToken = LinkTokenInterface(_link);
+    }
+    function whitelistChain(
+        uint64 _destinationChainSelector
+    ) external onlyOwner {
+        whitelistedChains[_destinationChainSelector] = true;
+    }
+
+    function denylistChain(
+        uint64 _destinationChainSelector
+    ) external onlyOwner {
+        whitelistedChains[_destinationChainSelector] = false;
+    }
+    
+    function transferTokens(
+        uint64 _destinationChainSelector,
+        address _receiver,
+        address _token,
+        uint256 _amount
+    ) 
+        external
+        onlyOwner
+        onlyWhitelistedChain(_destinationChainSelector)
+        returns (bytes32 messageId)
+    {
+        Client.EVMTokenAmount[]
+            memory tokenAmounts = new Client.EVMTokenAmount[](1);
+        Client.EVMTokenAmount memory tokenAmount = Client.EVMTokenAmount({
+            token: _token,
+            amount: _amount
+        });
+        tokenAmounts[0] = tokenAmount;
+        
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver),
+            data: "",
+            tokenAmounts: tokenAmounts,
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV1({gasLimit: 0, strict: false})
+            ),
+            feeToken: address(linkToken)
+        });
+        
+        uint256 fees = router.getFee(_destinationChainSelector, message);
+
+        if (fees > linkToken.balanceOf(address(this)))
+            revert NotEnoughBalance(linkToken.balanceOf(address(this)), fees);
+
+        linkToken.approve(address(router), fees);
+        
+        IERC20(_token).approve(address(router), _amount);
+        
+        messageId = router.ccipSend(_destinationChainSelector, message);
+        
+        emit TokensTransferred(
+            messageId,
+            _destinationChainSelector,
+            _receiver,
+            _token,
+            _amount,
+            address(linkToken),
+            fees
+        );
+    }
 }
